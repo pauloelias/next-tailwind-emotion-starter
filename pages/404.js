5f4a52126db75ecf30877664a40bca4ee6cd8fe0
Paulo Elias <paulo@pauloelias.com>
2020-09-25 11:02:25 -0700

    Improve custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,32 @@
+import Head from 'next/head'
+import tw from 'twin.macro'
+
+const Wrapper = tw.div`
+  flex items-center justify-center flex-col h-screen
+`
+
+const Main = tw.div`
+  p-6 bg-gray-100 rounded-lg shadow-2xl
+`
+
+const Heading = tw.h1`
+  text-2xl text-gray-500 uppercase
+`
+
+const Text = tw.p`
+  text-xl text-gray-700
+`
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+    <Wrapper>
+      <Head>
+        <title>Page Not Found</title>
+      </Head>
+      <Main>
+        <Heading>Oops!</Heading>
+        <Text>We can't find the page that you are looking for.</Text>
+      </Main>
+    </Wrapper>
+  )
 }
